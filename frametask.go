35be039ba2b87b2c6eea5d5ebc75b8aa1f4a877f
SeukWon Kang <kasworld@gmail.com>
2015-05-22 16:29:20 +0900

    apply typed enum

--- frametask.go
+++ frametask.go
@@ -1,22 +1,35 @@
+// Copyright 2015 SeukWon Kang (kasworld@gmail.com)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // task manage by framestep
 package frametask
 
 import (
 	"fmt"
 	"sync"
+
+	"github.com/kasworld/idgen"
 )
 
-type taskI interface {
-	GetID() int64
+type TaskI interface {
+	GetID() idgen.IDInt
 	SetFrame(f int64)
 	GetFrame() int64
 }
 
-type TaskList []taskI
+type TaskList []TaskI
 
 type TaskQueue struct {
 	frame2task map[int64]TaskList
-	id2task    map[int64]taskI
+	id2task    map[idgen.IDInt]TaskI
 	mutex      sync.Mutex
 }
 
@@ -27,11 +40,11 @@ func (ftq TaskQueue) String() string {
 func New() *TaskQueue {
 	return &TaskQueue{
 		frame2task: make(map[int64]TaskList),
-		id2task:    make(map[int64]taskI),
+		id2task:    make(map[idgen.IDInt]TaskI),
 	}
 }
 
-func (ftq *TaskQueue) AddTaskToFrame(task taskI, step int64) {
+func (ftq *TaskQueue) AddTaskToFrame(task TaskI, step int64) {
 	ftq.mutex.Lock()
 	defer ftq.mutex.Unlock()
 	task.SetFrame(step)
@@ -56,11 +69,11 @@ func (ftq *TaskQueue) ClearFrame(step int64) {
 	delete(ftq.frame2task, step)
 }
 
-func (ftq *TaskQueue) GetTaskByID(id int64) taskI {
+func (ftq *TaskQueue) GetTaskByID(id idgen.IDInt) TaskI {
 	return ftq.id2task[id]
 }
 
-func (ftq *TaskQueue) CancelTaskByID(id int64) taskI {
+func (ftq *TaskQueue) CancelTaskByID(id idgen.IDInt) TaskI {
 	ftq.mutex.Lock()
 	defer ftq.mutex.Unlock()
 	task := ftq.GetTaskByID(id)
